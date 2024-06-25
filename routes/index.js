c91e2b93e991f40b334a982f75b6cba4723889aa
FabioJamaL <fabiojamaral@hotmail.com.br>
2024-06-25 17:22:01 -0300

    .

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'FabioJamaraL@hotmail.com.br',
+    date: new Date().toLocaleDateString(),
+    time: new Date().toLocaleTimeString()    
+  });
 });
 
 module.exports = router;
