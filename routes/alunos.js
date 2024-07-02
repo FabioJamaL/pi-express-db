c45c3367a05dbc1c3dfd5015e2efe5615b878147
FabioJamaL <fabiojamaral@hotmail.com.br>
2024-07-02 16:43:04 -0300

    confirmado

--- routes/alunos.js
+++ routes/alunos.js
@@ -4,7 +4,11 @@ var alunos = require('../tests/mocks/alunos.json');
 
 /* GET home page. */
 router.get('/', function (_req, res, next) {
-    const data = {title: 'Alunos', alunos };
+    const data = {
+        title: 'Alunos', 
+        alunos: mockData.content
+    };
+
     res.render('list', data);
 });
 module.exports = router;
